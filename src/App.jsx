a2b4dbd16500d07b58f2f7f527c103128a898dfc
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2024-04-18 14:17:04 -0700

    initial push

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <h2>World</h2>
+      <img src="https://placebacon.net/400/300" alt="" />
+      <hr/>
+    </>
+  )
+}
+
+export default App
